aeb503769532a295c74cc4a4adaa672aaba976fb
Leonan <pedroleonan85@gmail.com>
2023-08-15 17:40:19 -0300

    Att home de position para flexbox

--- js/main.js
+++ js/main.js
@@ -3,35 +3,48 @@ const navbar = document.querySelector('header');
 // Adiciona um ouvinte de evento para o evento "scroll" na janela
 window.addEventListener('scroll', function() {
   if (window.scrollY > 0) {
-    navbar.classList.add('animar');
+    navbar.classList.add('animar')
   } else {
-    navbar.classList.remove('animar');
+    navbar.classList.remove('animar')
   }
 });
 
-const slider = document.querySelector('.slider');
-const navButtons = document.querySelectorAll('.slider-button');
-let currentIndex = 0;
+const slider = document.querySelector('.slider')
+const navButtons = document.querySelectorAll('.slider-button')
+let currentIndex = 0
 
 window.addEventListener('load', startSlider);
 
 navButtons.forEach((button, index) => {
   button.addEventListener('click', () => {
-    currentIndex = index;
-    showImage();
+    currentIndex = index
+    resetInterval()
+    showImage()
   });
 });
 
 function startSlider() {
-  setInterval(() => {
-    if(currentIndex==2){
-      currentIndex=-1;
-    }
-    showImage();
-  }, 2000);
+  intervalId = setInterval(() => {
+    currentIndex = (currentIndex+1)%3
+    showImage()
+  }, 2000)
+}
+
+function resetInterval(){
+  clearInterval(intervalId);
+  intervalId = setInterval(() => {
+    currentIndex = (currentIndex + 1) % 3
+    showImage()
+  }, 2000)
 }
 
 function showImage() {
-  currentIndex++;
   slider.style.backgroundImage = `url(/assets/img-main-${currentIndex+1}.jpg)`
+  navButtons.forEach((button, index) => {
+    if (index === currentIndex) {
+      button.classList.add('active')
+    } else {
+      button.classList.remove('active')
+    }
+  })
 }
